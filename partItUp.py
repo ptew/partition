9783daa8da953b29c9a6a6e05b9c30fb9dce2814
Parker Tew <parkertew@gmail.com>
2017-07-11 08:54:45 -0400

    slight cleanup of partItUp

--- partItUp.py
+++ partItUp.py
@@ -38,33 +38,33 @@ cachedResults[3][3] = 1
 cachedResults[3][2] = 1
 cachedResults[3][1] = 3
 
-def recursivePartition(n,maxSize):
+# n is the number of items to partition
+# minSize is the smallest block size that can be used
+def recursiveParition(n,minSize):
     if n in cachedResults:
-        if maxSize in cachedResults[n]:
-            return cachedResults[n][maxSize]
+        if minSize in cachedResults[n]:
+            return cachedResults[n][minSize]
     else:
         cachedResults[n] = {}
 
-    cachedResults[n][maxSize] = 0
-    if n == maxSize:
-        cachedResults[n][maxSize] = 1
-    elif n >= 2 * maxSize:
-        for subSize in range(maxSize,0,-1):
+    cachedResults[n][minSize] = 0
+    if n == minSize:
+        cachedResults[n][minSize] = 1
+    elif n >= 2 * minSize:
+        for subSize in range(minSize,0,-1):
             print "n: %d subsize: %d" % (n, subSize)
-            cachedResults[n][maxSize] += recursivePartition(n,subSize+1) + recursivePartition(n-subSize,subSize)
+            cachedResults[n][minSize] += recursivePartition(n,subSize+1) + recursivePartition(n-subSize,subSize)
     else:
-        cachedResults[n][maxSize] = recursivePartition(n,maxSize+1)
-    return cachedResults[n][maxSize]
+        cachedResults[n][minSize] = recursivePartition(n,minSize+1)
+    return cachedResults[n][minSize]
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		print partition(int(sys.argv[1]))
 	else:
-		#answers = [1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490, 627, 792, 1002, 1255, 1575, 1958, 2436, 3010, 3718, 4565, 5604]
-		answers = [1, 1, 2, 3, 5]#, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490, 627, 792, 1002, 1255, 1575, 1958, 2436, 3010, 3718, 4565, 5604]
+		answers = [1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490, 627, 792, 1002, 1255, 1575, 1958, 2436, 3010, 3718, 4565, 5604]
 
 		for index in range(len(answers)):
-			#result = partition(index)
-			result = recursivePartition(index,1)
-			passed = result == answers[index]
-			print "Passed: %d Index: %d Expected: %d  Answer: %d" % (passed, index, answers[index], result)
+		  result = partition(index)
+		  passed = result == answers[index]
+		  print "Passed: %d Index: %d Expected: %d  Answer: %d" % (passed, index, answers[index], result)
--- partitions.py
+++ partitions.py
@@ -39,51 +39,50 @@ def partition(n):
     results.append(result)
   return results[n]
 
-cachedResults = {}
-def recursiveParition(n, minBlockSize):
-  if
-  elif n == 0:
-    return 1
-  elif n == 1:
-    cachedResults[1]    = {}
-    cachedResults[1][1] = 1
-    return 1
-  elif n == 2:
-    cachedResults[2]    = {}
-    cachedResults[2][2] = 1
-    cachedResults[2][1] = 2
-    return 2
-  elif n == 3:
-    cachedResults[3]    = {}
-    cachedResults[3][3] = 1
-    cachedResults[3][2] = 1
-    cachedResults[3][1] = 3
-    return 3
-
-
-  result = 0
-  cachedResults[n] = {}
-  for minBlock in range(minBlockSize,n):
-    subProblemLength = n - minBlock
-    if (subProblemLength > 1) and subProblemLength >= minBlock:
-      cachedResults[subProblemLength][minBlock] = recursiveParition(subProblemLength, minBlock)
-      result += cachedResults[subProblemLength][minBlock]
-    elif subProblemLength == minBlockSize:
-      result += 1
-
-  cachedResults[n][minBlockSize] = result
-  print cachedResults
-
-  return result
+# cachedResults = {}
+# def recursiveParition(n, minBlockSize):
+#   if
+#   elif n == 0:
+#     return 1
+#   elif n == 1:
+#     cachedResults[1]    = {}
+#     cachedResults[1][1] = 1
+#     return 1
+#   elif n == 2:
+#     cachedResults[2]    = {}
+#     cachedResults[2][2] = 1
+#     cachedResults[2][1] = 2
+#     return 2
+#   elif n == 3:
+#     cachedResults[3]    = {}
+#     cachedResults[3][3] = 1
+#     cachedResults[3][2] = 1
+#     cachedResults[3][1] = 3
+#     return 3
+# 
+# 
+#   result = 0
+#   cachedResults[n] = {}
+#   for minBlock in range(minBlockSize,n):
+#     subProblemLength = n - minBlock
+#     if (subProblemLength > 1) and subProblemLength >= minBlock:
+#       cachedResults[subProblemLength][minBlock] = recursiveParition(subProblemLength, minBlock)
+#       result += cachedResults[subProblemLength][minBlock]
+#     elif subProblemLength == minBlockSize:
+#       result += 1
+# 
+#   cachedResults[n][minBlockSize] = result
+#   print cachedResults
+# 
+#   return result
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		print partition(int(sys.argv[1]))
 	else:
-		#answers = [1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490, 627, 792, 1002, 1255, 1575, 1958, 2436, 3010, 3718, 4565, 5604]
-		answers = [1, 1, 2, 3, 5, 7, 11]#, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490, 627, 792, 1002, 1255, 1575, 1958, 2436, 3010, 3718, 4565, 5604]
+		answers = [1, 1, 2, 3, 5, 7, 11, 15, 22, 30, 42, 56, 77, 101, 135, 176, 231, 297, 385, 490, 627, 792, 1002, 1255, 1575, 1958, 2436, 3010, 3718, 4565, 5604]
 
 		for index in range(len(answers)):
-			result = recursiveParition(index,1)
+			result = partition(index)
 			passed = result == answers[index]
 			print "Passed: %d Index: %d Expected: %d  Answer: %d" % (passed, index, answers[index], result)
